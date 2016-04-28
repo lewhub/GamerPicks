6de2a6590e93252dc863a9e5c920c5e46a271b31
lewhub <bracey.lewis@gmail.com>
2016-04-28 14:10:21 -0700

    added about page and working on json data for games

--- server.js
+++ server.js
@@ -13,31 +13,12 @@ var
   cors = require('cors'),
   https = require('https')
 
-  var options = {
-  host: "www.igdb.com",
-  path: "/api/v1/games",
-  headers: { "Accept": "application/json", "Authorization" : 'Token token="G5HDgxEsCEb0QRrZEIUtt3zgdcqbm-Ib3gqRa0MdO1s"' }
-  }
-
-  var callback = function(response){
-  var dataResults = ''
-
-  response.on('data', function(data){
-    dataResults += data
-    // dataResults += data
-  })
-  response.on('end', function(){
-    console.log(dataResults.split(',{"id":')[3])
-    // console.log(dataResults.split('}'))
-  })
-  }
 // console.log(9000, process.env)
 var LOCAL_URL = 'mongodb://localhost/gamerpicks'
 
 mongoose.connect(process.env.DB_URL, function(err){
   if (err) throw err
   console.log('connected to mongodb')
-  https.request(options, callback).end()
 
 })
 
@@ -45,17 +26,6 @@ app.use(body_parser.urlencoded({extended: false}))
 app.use(body_parser.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
-
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     if (req.method === 'Options') {
-//       res.send(200);
-//     } else {
-//       return next();
-//     }
-//   })
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'public/index.html'))
@@ -68,5 +38,5 @@ app.use('/api/categories', category_routes)
 
 app.listen(process.env.PORT, function(err){
   if (err) throw err
-  console.log('listening on port 3000')
+  console.log('listing on port')
 })
